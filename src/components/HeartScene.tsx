deca7463e9f0eda3c24789df17de8a826c2524da
navaelll <luvyqt@gmail.com>
2026-06-21 02:07:20 +0700

    Update HeartScene.tsx

--- src/components/HeartScene.tsx
+++ src/components/HeartScene.tsx
@@ -36,7 +36,7 @@ const HeartCloud = () => {
   const particles = useMemo(() => {
     const temp = [];
     const count = 250; // Optimized count
-    const text = "love you jeyaa";
+    const text = "i love you so much jeya";
     
     for (let i = 0; i < count; i++) {
         const t = Math.random() * Math.PI * 2;
